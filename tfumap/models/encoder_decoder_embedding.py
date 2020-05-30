b9675ca48cd69c6ae700e671892e49831d6d5350
Timothy J Sainburg <tsainbur@smloaner.ucsd.edu>
2020-05-30 16:57:48 -0700

    laying groundwork for supervised

--- tfumap/models/encoder_decoder_embedding.py
+++ tfumap/models/encoder_decoder_embedding.py
@@ -8,11 +8,8 @@ from tqdm.autonotebook import tqdm
 
 tf.get_logger().setLevel("INFO")
 
-from tfumap.general import (
-    convert_distance_to_probability,
-    compute_cross_entropy,
-    get_graph_elements,
-)
+from umap.umap_ import make_epochs_per_sample
+from scipy.sparse import csr_matrix
 
 from datetime import datetime
 from sklearn.utils import check_random_state
@@ -26,14 +23,16 @@ class UMAP_neural_network(UMAP_tensorflow):
     def __init__(
         self,
         optimizer=None,
-        tensorboard_logdir=None,
+        tensorboard_logdir=None,  # directory for tensorboard log
         batch_size=None,  # size of batch used for batch training
         dims=None,  # dimensionality of data, if not flat (e.g. images for ConvNet)
         negative_sample_rate=2,  # how many negative samples per positive samples for training
         max_epochs_per_sample=None,  # (setting this value to 1 is equivalent to computing UMAP on nearest neighbors graph without fuzzy_simplicial_set)
         direct_embedding=False,  # whether to learn embeddings directly, or use neural network
+        use_classifier=False,  # whether a classifier network should be jointly trained with data
         encoder=None,  # the neural net used for encoding (defaults to 3 layer 100 neuron fc)
         decoder=None,  # the neural net used for decoding (defaults to 3 layer 100 neuron fc)
+        classifier=None,  # the neural net used for decoding (defaults to 3 layer 100 neuron fc)
         training_epochs=50,  # number of epochs to train for
         decoding_method=None,  # how to decode "autoencoder", "network", or None
         valid_X=None,  # validation data for reconstruction and classification error
@@ -597,3 +596,73 @@ class UMAP_neural_network(UMAP_tensorflow):
 
         else:
             return self.original_inverse_transform(X)
+
+
+def convert_distance_to_probability(distances, a, b):
+    """ convert distance representation into probability, 
+        as a function of a, b params
+    """
+    return 1.0 / (1.0 + a * distances ** (2 * b))
+
+
+def compute_cross_entropy(
+    probabilities_graph, probabilities_distance, EPS=1e-4, negative_sample_scale=1.0
+):
+    """ Compute cross entropy between low and high probability
+    """
+    # cross entropy
+    attraction_term = -probabilities_graph * tf.math.log(
+        tf.clip_by_value(probabilities_distance, EPS, 1.0)
+    )
+    repellant_term = (
+        -(1.0 - probabilities_graph)
+        * tf.math.log(tf.clip_by_value(1.0 - probabilities_distance, EPS, 1.0))
+        * negative_sample_scale
+    )
+
+    # balance the expected losses between atrraction and repel
+    CE = attraction_term + repellant_term
+    return attraction_term, repellant_term, CE
+
+
+def remove_redundant_edges(graph):
+
+    edges = graph.nonzero()
+    # remove redundancies
+    redundant_edges = edges[0] < edges[1]
+    edges = (edges[0][redundant_edges], edges[1][redundant_edges])
+    # reconstruct graph
+    graph = csr_matrix(
+        (np.array(graph[edges]).flatten(), (edges[0], edges[1])), shape=graph.shape
+    )
+
+    return graph
+
+
+def get_graph_elements(graph_, n_epochs):
+
+    graph_ = remove_redundant_edges(graph_)
+
+    ### should we remove redundancies here??
+    graph = graph_.tocoo()
+    graph.sum_duplicates()
+
+    n_vertices = graph.shape[1]
+
+    if n_epochs is None:
+        # For smaller datasets we can use more epochs
+        if graph.shape[0] <= 10000:
+            n_epochs = 500
+        else:
+            n_epochs = 200
+
+    graph.data[graph.data < (graph.data.max() / float(n_epochs))] = 0.0
+    graph.eliminate_zeros()
+
+    epochs_per_sample = make_epochs_per_sample(graph.data, n_epochs)
+
+    head = graph.row
+    tail = graph.col
+    weight = graph.data
+
+    return graph, epochs_per_sample, head, tail, weight, n_vertices
