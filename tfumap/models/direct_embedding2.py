3a4ace9c70efaed5b4d1fd47ae5d00223886f31f
Timothy J Sainburg <tsainbur@smloaner.ucsd.edu>
2020-05-30 16:06:33 -0700

    integrated direct embedding with network embedding

--- tfumap/models/direct_embedding2.py
+++ tfumap/models/direct_embedding2.py
@@ -23,28 +23,59 @@ from umap.spectral import spectral_layout
 class UMAP_tensorflow_direct_embedding(UMAP_tensorflow):
     def __init__(
         self,
-        optimizer=tf.keras.optimizers.Adadelta(50),
+        optimizer=tf.keras.optimizers.Adam(1e-3),
         tensorboard_logdir=None,
-        batch_size=None,
+        batch_size=None,  # size of batch used for batch training
+        dims=None,  # dimensionality of data, if not flat (e.g. images for ConvNet)
+        negative_sample_rate=2,  # how many negative samples per positive samples for training
         max_epochs_per_sample=10,
-        negative_sample_rate=2,
-        training_epochs=200,
+        direct_embedding=False,  # whether to learn embeddings directly, or use neural network
+        encoder=None,  # the neural net used for encoding (defaults to 3 layer 100 neuron fc)
+        decoder=None,  # the neural net used for decoding (defaults to 3 layer 100 neuron fc)
+        training_epochs=50,  # number of epochs to train for
+        decoding_method=None,  # how to decode "autoencoder", "network", or None
+        valid_X=None,  # validation data for reconstruction and classification error
+        valid_Y=None,  # validation labels for reconstruction and classification error
         *args,
         **kwargs
     ):
         # retrieve everything from base model
         super(UMAP_tensorflow_direct_embedding, self).__init__(*args, **kwargs)
 
-        self.batch_size = (
-            batch_size  # number of elements per batch for tensorflow embedding
-        )
+        self.batch_size = batch_size
+
         self.max_epochs_per_sample = (
             max_epochs_per_sample  # maximum number of repeated edges during training
         )
+
         #   (higher = more memory load, lower = less difference between low and high probabilility)
         self.optimizer = optimizer
 
         self.random_state = check_random_state(None)
+
+        self.dims = dims  # if this is an image, we should reshape for network
+        self.encoder = encoder  # neural network used for embedding
+        self.decoder = decoder  # neural network used for decoding
+        # whether to use a decoder, and what type  ("autoencoder", "network", None)
+        self.decoding_method = decoding_method
+
+        # whether to encode with encoder, or embed directly
+        self.direct_embedding = direct_embedding
+
+        if (self.decoding_method == "autoencoder") & direct_embedding:
+            # this could be supported if we passed X to the iterator
+            raise NotImplementedError(
+                "Autoencoder loss not supported for direct embedding"
+            )
+
+        self.valid_X = (
+            valid_X  # validation data used for reconstruction or classification
+        )
+        self.valid_Y = valid_Y  # validation labels used for classification
+
+        # make a binary cross entropy object for reconstruction
+        if self.decoding_method in ["autoencoder", "network"]:
+            self.binary_cross_entropy = tf.keras.losses.BinaryCrossentropy()
 
         # number of training epochs. Different than n_epochs for base model
         #   (which is still used, e.g. for embedding using original method)
@@ -57,7 +88,12 @@ class UMAP_tensorflow_direct_embedding(UMAP_tensorflow):
             )
         else:
             self.tensorboard_logdir = tensorboard_logdir
-        self.summary_writer = tf.summary.create_file_writer(self.tensorboard_logdir)
+        self.summary_writer_train = tf.summary.create_file_writer(
+            self.tensorboard_logdir + "/train"
+        )
+        self.summary_writer_valid = tf.summary.create_file_writer(
+            self.tensorboard_logdir + "/valid"
+        )
 
     def init_embedding_from_graph(self, init="spectral"):
         if isinstance(init, str) and init == "random":
@@ -99,6 +135,56 @@ class UMAP_tensorflow_direct_embedding(UMAP_tensorflow):
 
         return embedding
 
+    def compute_umap_loss(self, batch_to, batch_from):
+        """
+        compute the cross entropy loss for learning embeddings
+        """
+
+        if self.direct_embedding:
+            # get the embeddings
+            embedding_to = tf.gather(self.embedding, batch_to)
+            embedding_from = tf.gather(self.embedding, batch_from)
+        else:
+            # encode
+            embedding_to = self.encoder(batch_to)
+            embedding_from = self.encoder(batch_from)
+
+        # embeddings for negative samples
+        embedding_neg_to = tf.gather(
+            self.embedding, tf.repeat(batch_to, self.negative_sample_rate)
+        )
+        embedding_neg_from = tf.gather(
+            self.embedding,
+            tf.random.shuffle(tf.repeat(batch_from, self.negative_sample_rate)),
+        )
+
+        #  distances between samples
+        distance_embedding = tf.concat(
+            [
+                tf.norm(embedding_to - embedding_from, axis=1),
+                tf.norm(embedding_neg_to - embedding_neg_from, axis=1),
+            ],
+            axis=0,
+        )
+
+        # convert probabilities to distances
+        probabilities_distance = convert_distance_to_probability(
+            distance_embedding, self._a, self._b
+        )
+
+        # treat positive samples as p=1, and negative samples as p=0
+        probabilities_graph = tf.concat(
+            [tf.ones(embedding_to.shape[0]), tf.zeros(embedding_neg_to.shape[0])],
+            axis=0,
+        )
+
+        # cross entropy loss
+        (attraction_loss, repellant_loss, ce_loss) = compute_cross_entropy(
+            probabilities_graph, probabilities_distance, negative_sample_scale=1.0,
+        )
+
+        return ce_loss, embedding_to, embedding_from
+
     def batch_epoch_edges(self, edges_to, edges_from):
         """ permutes and batches edges for epoch
         """
@@ -138,54 +224,21 @@ class UMAP_tensorflow_direct_embedding(UMAP_tensorflow):
 
         return iter(edge_iter)
 
-    def compute_umap_loss(self, batch_to, batch_from):
-        """
-        compute the cross entropy loss for learning embeddings
-        """
-        # get the embeddings
-        embedding_to = tf.gather(self.embedding, batch_to)
-        embedding_from = tf.gather(self.embedding, batch_from)
+    @tf.function
+    def train_direct(self, batch_to, batch_from):
+        with tf.GradientTape() as tape:
+            ce_loss, _, _ = self.compute_umap_loss(batch_to, batch_from)
 
-        # embeddings for negative samples
-        embedding_neg_to = tf.gather(
-            self.embedding, tf.repeat(batch_to, self.negative_sample_rate)
-        )
-        embedding_neg_from = tf.gather(
-            self.embedding,
-            tf.random.shuffle(tf.repeat(batch_from, self.negative_sample_rate)),
-        )
+        grads = tape.gradient(ce_loss, [self.embedding])
+        grads = [tf.clip_by_value(grad, -4.0, 4.0) * self.alpha for grad in grads]
+        self.optimizer.apply_gradients(zip(grads, [self.embedding]))
 
-        #  distances between samples
-        distance_embedding = tf.concat(
-            [
-                tf.norm(embedding_to - embedding_from, axis=1),
-                tf.norm(embedding_neg_to - embedding_neg_from, axis=1),
-            ],
-            axis=0,
-        )
-
-        # convert probabilities to distances
-        probabilities_distance = convert_distance_to_probability(
-            distance_embedding, self._a, self._b
-        )
-
-        # treat positive samples as p=1, and negative samples as p=0
-        probabilities_graph = tf.concat(
-            [tf.ones(embedding_to.shape[0]), tf.zeros(embedding_neg_to.shape[0])],
-            axis=0,
-        )
-
-        # cross entropy loss
-        (attraction_loss, repellant_loss, ce_loss) = compute_cross_entropy(
-            probabilities_graph, probabilities_distance, negative_sample_scale=1.0,
-        )
-
-        return ce_loss
+        return ce_loss, [0]
 
     @tf.function
     def train(self, batch_to, batch_from):
         with tf.GradientTape() as tape:
-            ce_loss = self.compute_umap_loss(batch_to, batch_from)
+            ce_loss, _, _ = self.compute_umap_loss(batch_to, batch_from)
 
         grads = tape.gradient(ce_loss, [self.embedding])
         grads = [tf.clip_by_value(grad, -4.0, 4.0) * self.alpha for grad in grads]
@@ -228,12 +281,7 @@ class UMAP_tensorflow_direct_embedding(UMAP_tensorflow):
         for edge_epoch, epoch in iter_:
             # loop through batches
             for batch_to, batch_from in edge_epoch:
-                ce_loss = self.train(batch_to, batch_from)
-                with self.summary_writer.as_default():
-                    tf.summary.scalar("loss/train", tf.reduce_mean(ce_loss), step=epoch)
-                    self.summary_writer.flush()
-        self.summary_writer.close()
-
+                ce_loss, _ = self.train_direct(batch_to, batch_from)
         if self.verbose:
             print(ts() + " Finished embedding")
 
