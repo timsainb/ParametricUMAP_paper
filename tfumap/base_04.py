c879c0bb1905ed87192681d75c6e39e2ec358c6e
Timothy J Sainburg <tsainbur@smloaner.ucsd.edu>
2020-07-03 12:53:34 -0700

    fixed epochs per exemplar

--- tfumap/base_04.py
+++ tfumap/base_04.py
@@ -5,10 +5,6 @@ that splits fit() into fit_generate_graph() and embed_graph()
 this way, we can create children of this object that only change the embedder. 
 """
 
-
-# Author: Leland McInnes <leland.mcinnes@gmail.com>
-#
-# License: BSD 3 clause
 from __future__ import print_function
 
 import locale
@@ -18,7 +14,6 @@ import time
 from scipy.optimize import curve_fit
 from sklearn.base import BaseEstimator
 from sklearn.utils import check_random_state, check_array
-from sklearn.utils.validation import check_is_fitted
 from sklearn.metrics import pairwise_distances
 from sklearn.preprocessing import normalize
 from sklearn.neighbors import KDTree
@@ -38,36 +33,7 @@ import numba
 import umap.distances as dist
 
 import umap.sparse as sparse
-
-from umap.utils import (
-    tau_rand_int,
-    deheap_sort,
-    submatrix,
-    ts,
-    csr_unique,
-    fast_knn_indices,
-)
-from umap.spectral import spectral_layout
-from umap.utils import deheap_sort, submatrix
-from umap.layouts import (
-    optimize_layout_euclidean,
-    optimize_layout_generic,
-    optimize_layout_inverse,
-)
-
-from pynndescent import NNDescent
-from pynndescent.distances import named_distances as pynn_named_distances
-from pynndescent.sparse import sparse_named_distances as pynn_sparse_named_distances
-
-locale.setlocale(locale.LC_NUMERIC, "C")
-
-INT32_MIN = np.iinfo(np.int32).min + 1
-INT32_MAX = np.iinfo(np.int32).max - 1
-
-SMOOTH_K_TOLERANCE = 1e-5
-MIN_K_DIST_SCALE = 1e-3
-NPY_INFINITY = np.inf
-
+import umap.sparse_nndescent as sparse_nn
 
 from umap import umap_
 from umap.umap_ import (
@@ -81,12 +47,56 @@ from umap.umap_ import (
     simplicial_set_embedding,
 )
 
+from umap.utils import (
+    tau_rand_int,
+    deheap_sort,
+    submatrix,
+    ts,
+    csr_unique,
+    fast_knn_indices,
+)
+from umap.rp_tree import rptree_leaf_array, make_forest
+from umap.nndescent import (
+    # make_nn_descent,
+    # make_initialisations,
+    # make_initialized_nnd_search,
+    nn_descent,
+    initialized_nnd_search,
+    initialise_search,
+)
+from umap.rp_tree import rptree_leaf_array, make_forest
+from umap.spectral import spectral_layout
+from umap.utils import deheap_sort, submatrix
+from umap.layouts import (
+    optimize_layout_euclidean,
+    optimize_layout_generic,
+    optimize_layout_inverse,
+)
+
+try:
+    # Use pynndescent, if installed (python 3 only)
+    from pynndescent import NNDescent
+    from pynndescent.distances import named_distances as pynn_named_distances
+    from pynndescent.sparse import sparse_named_distances as pynn_sparse_named_distances
+
+    _HAVE_PYNNDESCENT = True
+except ImportError:
+    _HAVE_PYNNDESCENT = False
+
+locale.setlocale(locale.LC_NUMERIC, "C")
+
+INT32_MIN = np.iinfo(np.int32).min + 1
+INT32_MAX = np.iinfo(np.int32).max - 1
+
+SMOOTH_K_TOLERANCE = 1e-5
+MIN_K_DIST_SCALE = 1e-3
+NPY_INFINITY = np.inf
+
 
 class UMAP_tensorflow(umap_.UMAP):
     def __init__(self, *args, **kwargs):
         # retrieve everything from base model
-        # super(UMAP_tensorflow, self).__init__(*args, **kwargs)
-        super().__init__(*args, **kwargs)
+        super(UMAP_tensorflow, self).__init__(*args, **kwargs)
 
     def fit_generate_graph(self, X, y=None):
         """Generate graph to fit X into an embedded space.
@@ -115,21 +125,9 @@ class UMAP_tensorflow(umap_.UMAP):
             self._a = self.a
             self._b = self.b
 
-        if isinstance(self.init, np.ndarray):
-            init = check_array(self.init, dtype=np.float32, accept_sparse=False)
-        else:
-            init = self.init
-
         self._initial_alpha = self.learning_rate
 
         self._validate_parameters()
-
-        # if self.verbose:
-        #   print(str(self))
-
-        self._original_n_threads = numba.get_num_threads()
-        if self.n_jobs > 0 and self.njobs is not None:
-            numba.set_num_threads(self.n_jobs)
 
         # Check if we should unique the data
         # We've already ensured that we aren't in the precomputed case
@@ -179,8 +177,6 @@ class UMAP_tensorflow(umap_.UMAP):
                 "X.shape[0] - 1"
             )
             self._n_neighbors = X[index].shape[0] - 1
-            if self.densmap:
-                self._densmap_kwds["n_neighbors"] = self._n_neighbors
         else:
             self._n_neighbors = self.n_neighbors
 
@@ -219,12 +215,7 @@ class UMAP_tensorflow(umap_.UMAP):
                 row_nn_data_indices = np.argsort(row_data)[: self._n_neighbors]
                 self._knn_indices[row_id] = row_indices[row_nn_data_indices]
                 self._knn_dists[row_id] = row_data[row_nn_data_indices]
-            (
-                self.graph_,
-                self._sigmas,
-                self._rhos,
-                self.graph_dists_,
-            ) = fuzzy_simplicial_set(
+            self.graph_, self._sigmas, self._rhos = fuzzy_simplicial_set(
                 X[index],
                 self.n_neighbors,
                 random_state,
@@ -237,7 +228,6 @@ class UMAP_tensorflow(umap_.UMAP):
                 self.local_connectivity,
                 True,
                 self.verbose,
-                self.densmap or self.output_dens,
             )
         # Handle small cases efficiently by computing all distances
         elif X[index].shape[0] < 4096 and not self.force_approximation_algorithm:
@@ -269,12 +259,7 @@ class UMAP_tensorflow(umap_.UMAP):
                         metric=self._input_distance_func,
                         kwds=self._metric_kwds,
                     )
-            (
-                self.graph_,
-                self._sigmas,
-                self._rhos,
-                self.graph_dists_,
-            ) = fuzzy_simplicial_set(
+            self.graph_, self._sigmas, self._rhos = fuzzy_simplicial_set(
                 dmat,
                 self._n_neighbors,
                 random_state,
@@ -287,24 +272,21 @@ class UMAP_tensorflow(umap_.UMAP):
                 self.local_connectivity,
                 True,
                 self.verbose,
-                self.densmap or self.output_dens,
             )
         else:
             # Standard case
             self._small_data = False
-            # Standard case
-            if self._sparse_data and self.metric in pynn_sparse_named_distances:
-                nn_metric = self.metric
-            elif not self._sparse_data and self.metric in pynn_named_distances:
-                nn_metric = self.metric
+            # pass string identifier if pynndescent also defines distance metric
+            if _HAVE_PYNNDESCENT:
+                if self._sparse_data and self.metric in pynn_sparse_named_distances:
+                    nn_metric = self.metric
+                elif not self._sparse_data and self.metric in pynn_named_distances:
+                    nn_metric = self.metric
+                else:
+                    nn_metric = self._input_distance_func
             else:
                 nn_metric = self._input_distance_func
-
-            (
-                self._knn_indices,
-                self._knn_dists,
-                self._knn_search_index,
-            ) = nearest_neighbors(
+            (self._knn_indices, self._knn_dists, self._rp_forest) = nearest_neighbors(
                 X[index],
                 self._n_neighbors,
                 nn_metric,
@@ -313,16 +295,10 @@ class UMAP_tensorflow(umap_.UMAP):
                 random_state,
                 self.low_memory,
                 use_pynndescent=True,
-                n_jobs=self.n_jobs,
                 verbose=self.verbose,
             )
 
-            (
-                self.graph_,
-                self._sigmas,
-                self._rhos,
-                self.graph_dists_,
-            ) = fuzzy_simplicial_set(
+            self.graph_, self._sigmas, self._rhos = fuzzy_simplicial_set(
                 X[index],
                 self.n_neighbors,
                 random_state,
@@ -335,17 +311,48 @@ class UMAP_tensorflow(umap_.UMAP):
                 self.local_connectivity,
                 True,
                 self.verbose,
-                self.densmap or self.output_dens,
             )
+
+            if not _HAVE_PYNNDESCENT:
+                self._search_graph = scipy.sparse.lil_matrix(
+                    (X[index].shape[0], X[index].shape[0]), dtype=np.int8
+                )
+                _rows = []
+                _data = []
+                for i in self._knn_indices:
+                    _non_neg = i[i >= 0]
+                    _rows.append(_non_neg)
+                    _data.append(np.ones(_non_neg.shape[0], dtype=np.int8))
+
+                self._search_graph.rows = _rows
+                self._search_graph.data = _data
+                self._search_graph = self._search_graph.maximum(
+                    self._search_graph.transpose()
+                ).tocsr()
+
+                if (self.metric != "precomputed") and (len(self._metric_kwds) > 0):
+                    # Create a partial function for distances with arguments
+                    _distance_func = self._input_distance_func
+                    _dist_args = tuple(self._metric_kwds.values())
+                    if self._sparse_data:
+
+                        @numba.njit()
+                        def _partial_dist_func(ind1, data1, ind2, data2):
+                            return _distance_func(ind1, data1, ind2, data2, *_dist_args)
+
+                        self._input_distance_func = _partial_dist_func
+                    else:
+
+                        @numba.njit()
+                        def _partial_dist_func(x, y):
+                            return _distance_func(x, y, *_dist_args)
+
+                        self._input_distance_func = _partial_dist_func
 
         # Currently not checking if any duplicate points have differing labels
         # Might be worth throwing a warning...
-        if y is not None:
-            if self.densmap:
-                raise NotImplementedError(
-                    "Supervised embedding is not supported with densMAP."
-                )
 
+        if y is not None:
             len_X = len(X) if not self._sparse_data else X.shape[0]
             if len_X != len(y):
                 raise ValueError(
@@ -438,33 +445,32 @@ class UMAP_tensorflow(umap_.UMAP):
                 # #                                        target_graph -
                 # #                                        product)
                 # self.graph_ = product
+                print("COMPUTING SIMPLICIAL SET INTERSECTION")
                 self.graph_ = general_simplicial_set_intersection(
                     self.graph_, target_graph, self.target_weight
                 )
                 self.graph_ = reset_local_connectivity(self.graph_)
-                self._supervised = True
-        else:
-            self._supervised = False
-
-        self.index__ = index
-        self.inverse__ = inverse
         return index, inverse
 
     def fit_embed_data(self, X, y, index, inverse):
+
+        if isinstance(self.init, np.ndarray):
+            init = check_array(self.init, dtype=np.float32, accept_sparse=False)
+        else:
+            init = self.init
 
         if self.n_epochs is None:
             n_epochs = 0
         else:
             n_epochs = self.n_epochs
 
-        if self.densmap or self.output_dens:
-            self._densmap_kwds["graph_dists"] = self.graph_dists_
-
         if self.verbose:
             print(ts(), "Construct embedding")
 
-        self.embedding_, aux_data = simplicial_set_embedding(
-            self._raw_data[self.index__],  # JH why raw data?
+        random_state = check_random_state(self.random_state)
+
+        self.embedding_ = simplicial_set_embedding(
+            self._raw_data[index],  # JH why raw data?
             self.graph_,
             self.n_components,
             self._initial_alpha,
@@ -477,25 +483,16 @@ class UMAP_tensorflow(umap_.UMAP):
             random_state,
             self._input_distance_func,
             self._metric_kwds,
-            self.densmap,
-            self._densmap_kwds,
-            self.output_dens,
             self._output_distance_func,
             self._output_metric_kwds,
             self.output_metric in ("euclidean", "l2"),
             self.random_state is None,
             self.verbose,
-        )
-
-        self.embedding_ = self.embedding_[self.inverse__]
-        if self.output_dens:
-            self.rad_orig_ = aux_data["rad_orig"][self.inverse__]
-            self.rad_emb_ = aux_data["rad_emb"][self.inverse__]
+        )[inverse]
 
         if self.verbose:
             print(ts() + " Finished embedding")
 
-        numba.set_num_threads(self._original_n_threads)
         self._input_hash = joblib.hash(self._raw_data)
 
     def fit(self, X, y=None):
